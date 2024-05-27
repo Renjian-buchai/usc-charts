a4222722f8ebc72c86069e76e9cda5d1c69f75dd
Segfault (Core dumped) <tyiwei0@gmail.com>
2024-05-27 20:10:30 +0800

    ADded the rest

---
+++ maimai/April Showers/RUN_ME.bat
@@ -0,0 +1,18 @@
+cd /d %~dp0
+del /f /s /q apr_nov.ksh
+del /f /s /q apr_adv.ksh
+del /f /s /q apr_exh.ksh
+del /f /s /q apr_mxm.ksh
+del /f /s /q Aprshowers.ogg
+del /f /s /q aprjac.png
+del /f /s /q versions.txt
+ren f4e07b28d51c65715664db9390b33133 apr_nov.ksh
+ren 0d94a17cb0a2d63ace2bd5fcfe83a0c7 apr_adv.ksh
+ren 220f5e54bfad31205016f136093c8142 apr_exh.ksh
+ren b99a29f915efdcc3647c6d6a245741eb apr_mxm.ksh
+ren 3bb0dc6b7c51ec7bbf88c024d365601a 3bb0dc6b7c51ec7bbf88c024d365601a.png
+ren 8ea9d3c8cdd4b904bbafa57a881ad5b7 8ea9d3c8cdd4b904bbafa57a881ad5b7.png
+ren c0e4c6250e38d30bbd065b4d43ac38dc c0e4c6250e38d30bbd065b4d43ac38dc.png
+ren d2038ca3f4d2ec5123f23ed9df31104e d2038ca3f4d2ec5123f23ed9df31104e.png
+ren 9fa081a8634fe9e631cd85762bb9ceb9 versions.txt
+del %0
